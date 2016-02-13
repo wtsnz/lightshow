cf89a2a7cad169946a317a993728b0e9f10db76b
Will Townsend <will@townsend.io>
2016-02-13 13:21:19 +1300

    Initial Commit

---
+++ lightshow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lightshow
+//
+//  Created by Will Townsend on 13/02/16.
+//  Copyright © 2016 Will Townsend. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ lightshow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lightshow
+//
+//  Created by Will Townsend on 13/02/16.
+//  Copyright © 2016 Will Townsend. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
