31b96547081a6c1c9453830f7258056a8564de41
Will Townsend <will@townsend.io>
2016-02-15 00:13:52 +1300

    Add line drawing + calculating y value

--- lightshow/ViewController.swift
+++ lightshow/ViewController.swift
@@ -17,6 +17,155 @@ class View: NSView {
     
 }
 
+class LineView2: NSView {
+    
+    var path: NSBezierPath = NSBezierPath()
+    var points: [CGFloat] = []
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func mouseDown(event: NSEvent) {
+        super.mouseDown(event)
+        
+        let point = self.convertPoint(event.locationInWindow, fromView: self.window!.contentView!)
+        self.path.lineToPoint(point)
+//        self.setNeedsDisplay(true)
+    }
+    
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+        
+        self.path.lineWidth = 3
+        NSColor.blackColor().set()
+        self.path.stroke()
+        
+    }
+    
+}
+
+class LineView : NSView {
+    
+    var points = [CGPoint]()
+    
+    var newLinear = NSBezierPath()
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        newLinear.moveToPoint(CGPointMake(0, 0))
+        
+        points.append(CGPointZero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawRect(dirtyRect: NSRect) {
+        NSColor.redColor().set()
+        
+        let path = NSBezierPath()
+        for (index, point) in self.points.enumerate() {
+            
+            if index == 0 {
+                path.moveToPoint(point)
+            } else {
+                path.lineToPoint(point)
+            }
+            
+        }
+        
+        path.lineWidth = 1
+        path.stroke()
+    }
+    
+    func removeLastPoint() {
+        self.points.removeLast()
+        needsDisplay = true
+    }
+    
+    override func mouseDown(theEvent: NSEvent) {
+        super.mouseDown(theEvent)
+        var lastPt = self.convertPoint(theEvent.locationInWindow, fromView: self.window!.contentView!)
+        lastPt.x -= frame.origin.x
+        lastPt.y -= frame.origin.y
+        
+        self.points.append(lastPt)
+        
+//        newLinear.lineToPoint(lastPt)
+        needsDisplay = true
+    }
+    
+    override func mouseDragged(theEvent: NSEvent) {
+        super.mouseDragged(theEvent)
+        
+//        newLinear.
+        
+        var newPt = self.convertPoint(theEvent.locationInWindow, fromView: self.window!.contentView!)
+        newPt.x -= frame.origin.x
+        newPt.y -= frame.origin.y
+        
+//        var lastPoint = self.points.last!
+//        lastPoint = newPt
+        self.points[self.points.count - 1] = newPt
+        
+//        let test = self.pointForXPosition(100)
+        
+//        newLinear.lineToPoint(newPt)
+        needsDisplay = true
+    }
+    
+    func brightnessForPosition(position: CGFloat) -> Float {
+        
+        return 0.5
+    }
+    
+    func pointForXPosition(x: CGFloat) -> CGPoint {
+        
+        if self.points.count == 0 {
+            return CGPointZero
+        }
+        
+        let lastPointIndex = self.points.count - 1
+        
+        if x <= 0.0 {
+            return self.points[0]
+        }
+        
+        if x >= self.points[lastPointIndex].x {
+            return self.points[lastPointIndex]
+        }
+        
+        var index = 1
+        
+        while index < self.points.count && x > self.points[index].x {
+            index++
+        }
+        
+        let point1 = self.points[index - 1]
+        let point2 = self.points[index]
+        
+        let dy = point2.y - point1.y
+        let dx = (point2.x - point1.x)
+        
+        let slope = dy / dx
+        
+        let y = (slope * (x - point1.x)) + point1.y
+        
+        NSLog("dy: \(dy), dx: \(dx), y: \(y)")
+        
+        return CGPointMake(x, y)
+    }
+    
+}
+
+
 class ViewController: NSViewController {
     
     @IBOutlet weak var scrollView: NSScrollView!
@@ -31,12 +180,20 @@ class ViewController: NSViewController {
         audioPlotView.shouldFill = true
         audioPlotView.shouldMirror = true
         audioPlotView.addSubview(self.waveformOverlayView)
+        audioPlotView.addSubview(self.lineOverlayView)
         return audioPlotView
     }()
     
     lazy var waveformOverlayView: WaveformOverlayView = {
         let waveformOverlayView = WaveformOverlayView(frame: CGRectMake(0, 0, 100, 100))
         return waveformOverlayView
+    }()
+    
+    lazy var lineOverlayView: LineView = {
+        let lineOverlayView = LineView(frame: CGRectMake(0, 0, 100, 100))
+        lineOverlayView.wantsLayer = true
+        lineOverlayView.layer?.backgroundColor = NSColor.blueColor().colorWithAlphaComponent(0.2).CGColor
+        return lineOverlayView
     }()
     
     override var acceptsFirstResponder: Bool {
@@ -62,6 +219,8 @@ class ViewController: NSViewController {
 
         let url = NSBundle.mainBundle().URLForResource("magnets", withExtension: "mp3")
         self.openFileWithFilePathURL(url!)
+        
+        
         
 //        dispatch_async(dispatch_get_main_queue()) { () -> Void in
 //        self.scrollView.addConstraint(NSLayoutConstraint(item: self.scrollView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 20))
@@ -96,6 +255,20 @@ class ViewController: NSViewController {
              self.playAction(self)
         }
     }
+    
+    
+    @IBAction func deleteLastLineAction(sender: AnyObject) {
+        
+        self.lineOverlayView.removeLastPoint()
+        
+    }
+    
+    @IBAction func toggleLineAction(sender: AnyObject) {
+        
+        self.lineOverlayView.hidden = !self.lineOverlayView.hidden
+        
+    }
+    
     
     @IBAction func playAction(sender: AnyObject) {
         
@@ -145,17 +318,13 @@ class ViewController: NSViewController {
             self.waveformOverlayView.totalFrames = audioFile.totalFrames
             
             dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                
-//                self.audioPlotView.frame = CGRectMake(0, 0, CGFloat(audioFile.duration * 30), self.scrollView.contentView.frame.height)
-                
-                
                 self.resizePlotView()
             }
             
         }
         
         self.audioPlayer = EZAudioPlayer(audioFile: self.audioFile!, delegate: self)
-        self.audioFile?.getWaveformDataWithNumberOfPoints(4096, completion: { (test: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, length: Int32) -> Void in
+        self.audioFile?.getWaveformDataWithNumberOfPoints(4096 + 4096, completion: { (test: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, length: Int32) -> Void in
             
             let te = test[0]
             self.audioPlotView.updateBuffer(te, withBufferSize: UInt32(length))
@@ -179,6 +348,7 @@ class ViewController: NSViewController {
         self.audioPlotView.frame = bounds
         self.scrollView.documentView?.setFrameSize(bounds.size)
         self.waveformOverlayView.frame = bounds
+        self.lineOverlayView.frame = bounds
     }
     
 }
@@ -194,8 +364,15 @@ extension ViewController: EZAudioPlayerDelegate {
         dispatch_async(dispatch_get_main_queue()) { () -> Void in
             self.waveformOverlayView.setCurrentPosition(framePosition)
             self.timeCodeLabel.stringValue = "\(audioPlayer.formattedCurrentTime) \(framePosition)"
-            let colour = colorForFrame(framePosition)
+            
+            let point = self.lineOverlayView.pointForXPosition(self.waveformOverlayView.indicatorLayer.frame.origin.x)
+            let value = ((100 / self.lineOverlayView.frame.height) * point.y) / 100
+            let colour = LFXHSBKColor(hue: 120, saturation: 1, brightness: value, kelvin: 3000)
+//            let colour = colorForFrame(framePosition)
             LFXClient.sharedClient().localNetworkContext.allLightsCollection.setColor(colour)
+            
+            
+            
         }
         
     }
@@ -206,6 +383,8 @@ extension ViewController: WaveformOverlayViewDelegate {
     
     func waveformOverlayViewDidUpdatePlaybackPosition(waveformOverlayView: WaveformOverlayView, position: Int64) {
 
+        
+        
         self.audioPlayer?.seekToFrame(position)
         
         
--- lightshow/WaveformOverlayView.swift
+++ lightshow/WaveformOverlayView.swift
@@ -39,13 +39,20 @@ class WaveformOverlayView: NSView {
     
     override func mouseDown(event: NSEvent) {
         super.mouseDown(event)
+        self.handleMouseEvent(event)
+    }
+    
+    override func mouseDragged(event: NSEvent) {
+        super.mouseDragged(event)
+        self.handleMouseEvent(event)
+    }
+    
+    func handleMouseEvent(event: NSEvent) {
         let point = self.convertPoint(event.locationInWindow, fromView: self.window!.contentView!)
-        
         let percent = (100 / self.bounds.width) * point.x
         let position = (CGFloat(self.totalFrames) * percent) / 100
         NSLog("boundswidth: \(self.bounds.width), point.x: \(point.x), percent: \(percent), position: \(position)")
         self.delegate?.waveformOverlayViewDidUpdatePlaybackPosition(self, position: Int64(position))
-        
     }
     
     func setCurrentPosition(position: Int64) {
@@ -56,16 +63,16 @@ class WaveformOverlayView: NSView {
         
         let x = CGFloat(Int(CGFloat(self.bounds.width / CGFloat(self.totalFrames)) * CGFloat(position)))
         
-//        if x != self.indicatorLayer.frame.origin.x {
+        if x != self.indicatorLayer.frame.origin.x {
 //            dispatch_async(dispatch_get_main_queue()) { () -> Void in
 //                NSLog("x: \(x)")
         CATransaction.begin()
         CATransaction.setDisableActions(true)
-                self.indicatorLayer.frame = CGRectMake(CGFloat(x), 0, 1, self.bounds.height)
+        self.indicatorLayer.frame = CGRectMake(CGFloat(x), 0, 1, self.bounds.height)
         CATransaction.commit()
 
 //            }
-//        }
+        }
         
         
     }
